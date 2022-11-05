732660b133ba348bb7c6c9ac27cb89001f0f675f
may huang <mayno1024@gmail.com>
2022-11-05 16:36:26 +0800

    Add index page

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 // require packeges
 const express = require ('express')
 const app = express()
+const exphbs = require('express-handlebars') 
 
 // define variables
 const port = 3000
 
+// set templete engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+// 這句是一定要的，但我不知道它的作用到底是甚麼。單看doc的話感覺有點像是在define。
+app.set('view engine', 'handlebars')
+
+// set static files 
+app.use(express.static('public'))
+
 // set routing 
 app.get('/', (req ,res) => {
-  res.send('restaurants list')
+  res.render('index')
 })
 
 // listen on server 
