137ec064147c928dabf60d5c26fb526b69877902
may huang <mayno1024@gmail.com>
2022-11-05 17:41:29 +0800

    Pass data into index page

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-// require packeges
+// require express
 const express = require ('express')
 const app = express()
+
+// set templete engine 
 const exphbs = require('express-handlebars') 
-
-// define variables
-const port = 3000
-
-// set templete engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 // 這句是一定要的，但我不知道它的作用到底是甚麼。單看doc的話感覺有點像是在define。
 app.set('view engine', 'handlebars')
+
+// define variables
+const port = 3000
+const data = require('./restaurant-data.json')
 
 // set static files 
 app.use(express.static('public'))
 
 // set routing 
-app.get('/', (req ,res) => {
-  res.render('index')
+app.get('/restaurants', (req ,res) => {
+  res.render('index',{restaurants: data.results})
 })
 
 // listen on server 
