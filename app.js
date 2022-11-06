e8839f3b1d7885e45ab1633857da4a65cb7fc890
may huang <mayno1024@gmail.com>
2022-11-06 21:08:48 +0800

    Make it measure up to JavaScript Standard

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // require express
-const express = require ('express')
+const express = require('express')
 const app = express()
 
-// set templete engine 
-const exphbs = require('express-handlebars') 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+// set templete engine
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 // 這句是一定要的，但我不知道它的作用到底是甚麼。單看doc的話感覺有點像是在define。
 app.set('view engine', 'handlebars')
 
@@ -12,19 +12,19 @@ app.set('view engine', 'handlebars')
 const port = 3000
 const data = require('./restaurant-data.json')
 
-// set static files 
+// set static files
 app.use(express.static('public'))
 
-// set routing 
-app.get('/restaurants', (req ,res) => {
-  res.render('index',{restaurants: data.results})
+// set routing
+app.get('/restaurants', (req, res) => {
+  res.render('index', { restaurants: data.results })
 })
 
 app.get('/restaurants/:id', (req, res) => {
   const restaurant = data.results.find(result => {
     return result.id.toString() === req.params.id
-  })  
-  res.render('show',{restaurant: restaurant})
+  })
+  res.render('show', { restaurant: restaurant })
 })
 
 app.get('/search', (req, res) => {
@@ -36,7 +36,7 @@ app.get('/search', (req, res) => {
     return result.category.includes(req.query.keyword)
   })
   const results = resultOfName.concat(resultOfCategory)
-  
+
   // 若使用者如果沒有在搜尋欄輸入內容或只有輸入空格
   // function check (str) {
   //   let textInSearchBar = str.trim()
@@ -45,12 +45,11 @@ app.get('/search', (req, res) => {
   //   }
   // }
   // check(keyword)
-  
-  res.render('index', {restaurants: results,keyword: keyword})
+
+  res.render('index', { restaurants: results, keyword: keyword })
 })
 
-// listen on server 
-app.listen(port,() => {
-  console.log(`listen on port: ${port}`)  
+// listen on server
+app.listen(port, () => {
+  console.log(`listen on port: ${port}`)
 })
-
