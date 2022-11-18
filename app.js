1cde614584fde3d775ae7a7fc2f11cefceb96314
may huang <mayno1024@gmail.com>
2022-11-18 14:46:58 +0800

    Create add restaurant function

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+// connect to db 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', ()=> {
+  console.log('mongodb error')
+})
+db.once('open', ()=>{
+  console.log('mongodb connect')
+})
+
 // require restaurant data 
 const Restaurant = require('./models/restaurant')
 
@@ -17,6 +29,11 @@ app.set('view engine', 'handlebars')
 
 // define variables
 const port = 3000
+
+// listen on server
+app.listen(port, () => {
+  console.log(`listen on port: ${port}`)
+})
 
 // set static files
 app.use(express.static('public'))
@@ -33,7 +50,7 @@ app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   return Restaurant.findById(id)
   .lean()
-  .then(restaurant => {res.render('show', { restaurant })})
+  .then(restaurant => res.render('show', { restaurant }))
   .catch(error => {console.log(error)})
 })
 
@@ -47,21 +64,28 @@ app.get('/search', (req, res) => {
     })
     res.render('index', { restaurants: filterData, keyword })
   })
+  .catch(err => console.log(err))
 })
 
-// listen on server
-app.listen(port, () => {
-  console.log(`listen on port: ${port}`)
+// set routing for create 
+app.get('/new', (req, res) => {
+  res.render('new')
 })
 
-// connect to db 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error', ()=> {
-  console.log('mongodb error')
-})
-db.once('open', ()=>{
-  console.log('mongodb connect')
+// set routing for catching data
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description})
+  .then(() => { res.redirect('/') })
+  .catch(error => console.log(error))
 })
