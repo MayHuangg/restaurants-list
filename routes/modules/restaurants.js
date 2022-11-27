edd1ba54badc7059eb4640c00a47259d279fd26f
may huang <mayno1024@gmail.com>
2022-11-27 17:04:18 +0800

    refactor: modify edit function

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -37,19 +37,9 @@ router.get('/:id/edit', (req, res) => {
 // pass data into db 
 router.put('/:id', (req, res) => {
   const id = req.params.id 
-  const {name, name_en, category, image, location, phone, google_map, rating, description} = req.body
-
   return Restaurant.findById(id)
     .then(restaurant => {
-      restaurant.name = name
-      restaurant.name_en = name_en
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = google_map
-      restaurant.rating = rating
-      restaurant.description = description
+      Object.assign(restaurant, req.body)
       return restaurant.save()
     })
     .then(() => res.redirect(`/restaurants/${id}`))
