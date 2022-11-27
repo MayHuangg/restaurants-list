565835704cacc6da29e756e42c7e7331ca88a56f
may huang <mayno1024@gmail.com>
2022-11-27 11:57:50 +0800

    refactor: modify seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -7,17 +7,5 @@ const db = require('../../config/mongoose')
 
 db.once('open', () => {
   const restaurantData = data.results
-  for(let i = 0; i < restaurantData.length; i++) {
-    Restaurant.create({
-      name: restaurantData[i].name,
-      name_en: restaurantData[i].name_en,
-      category: restaurantData[i].category,
-      image: restaurantData[i].image,
-      location: restaurantData[i].location,
-      phone: restaurantData[i].phone,
-      google_map: restaurantData[i].google_map,
-      rating: restaurantData[i].rating,
-      description: restaurantData[i].description
-    })
-  }
+  Restaurant.insertMany(restaurantData)
 })
